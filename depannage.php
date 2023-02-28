58ccc7c602e7fd02d2880ff0dd487c33afcb54dc
Lucas Didier <lucas.didier@hotmail.fr>
2023-02-28 09:48:19 +0100

    Up to date

---
+++ depannage.php
@@ -0,0 +1,11 @@
+<?php include("frontend/template/header.php"); ?>
+<?php include("frontend/template/navbar.php"); ?>
+
+<div class="container">    
+    <h1>Liste des dépanneurs près de chez vous</h1>
+    <?php
+
+    ?>
+</div>
+
+<?php include("frontend/template/footer.php"); ?>
